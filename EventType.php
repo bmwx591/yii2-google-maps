99b9a34205cf7b07328e6037b823557736977cb4
ivan <bmw_x591@mail.ru>
2015-06-22 18:16:42 +0300

    Commit

--- EventType.php
+++ EventType.php
@@ -4,7 +4,7 @@
  * @link http://2amigos.us
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  */
-namespace bbmx591\google\maps;
+namespace bmx591\google\maps;
 
 /**
  * EventType
